5cd6eb1a0d0521c4ae4dbbdad95207b65e06d69c
VictorJiangXin <921320448@qq.com>
2018-04-24 15:28:08 +0800

    add the doc and add the code of generate csv

--- Source/BitcoinTransactionGraphGenerator/BitcoinTransactionGraphGenerator/BitcoinAdoNetLayer.cs
+++ Source/BitcoinTransactionGraphGenerator/BitcoinTransactionGraphGenerator/BitcoinAdoNetLayer.cs
@@ -92,5 +92,72 @@ namespace BitcoinTransactionGraphGenerator
                                                      ORDER BY BitcoinTransactionId desc";
             return AdoNetLayer.ConvertDbValue<long>(adoNetLayer.ExecuteScalar(sqlCommandGetMaxTransactionId));
         }
+
+        public void UpdateTransactionOutputAddressId()
+        {
+            const string sqlGetFirstTXOutUnsetAddressId = @"
+                SELECT TOP 1 TransactionOutputId
+                FROM TransactionOutput
+                WHERE TransactionOutputId > @lastTransactionOutputId
+                AND OutputAddressId = -1
+                AND OutputAddress != '0'";
+
+            const string sqlUpdateAddressId = @"
+                UPDATE TransactionOutput
+                SET OutputAddressId = @mOutputAddressId
+                WHERE TransactionOutputId >= @currentTransactionOutputId
+                AND OutputAddress = (
+                    SELECT OutputAddress
+                    FROM TransactionOutput
+                    WHERE TransactionOutputId = @currentTransactionOutputId)";
+
+            const string sqlGetMaxOutputAddressId = @"
+                    SELECT MAX(OutputAddressId)
+                    FROM TransactionOutput";
+
+            long MaxOutputAddressId = AdoNetLayer.ConvertDbValue<long>(
+                this.adoNetLayer.ExecuteScalar(sqlGetMaxOutputAddressId));
+
+            long OutputAddressId = MaxOutputAddressId + 1;
+
+            //得到TransactionOutput的数量
+            long MaxTransactionOutputId = AdoNetLayer.ConvertDbValue<long>(
+                this.adoNetLayer.ExecuteScalar(
+                    @"select TOP 1 TransactionOutputId
+                      from TransactionOutput
+                      order by TransactionOutputId desc"));
+
+            long TransactionOutputCount = -1;
+
+            long SolveCount = 0;
+
+            Console.WriteLine("开始为地址编号......耗时也许会很久");
+            while (TransactionOutputCount < MaxTransactionOutputId)
+            {
+                //找到第一个OutputAddressId=-1 的 TxOutId即第一个未被编号的地址。
+                TransactionOutputCount = AdoNetLayer.ConvertDbValue<long>(this.adoNetLayer.ExecuteScalar(sqlGetFirstTXOutUnsetAddressId,
+                    AdoNetLayer.CreateInputParameter("@lastTransactionOutputId", SqlDbType.BigInt, TransactionOutputCount)));
+
+                this.adoNetLayer.ExecuteStatementNoResult(sqlUpdateAddressId,
+                    AdoNetLayer.CreateInputParameter("mOutputAddressId", SqlDbType.BigInt, OutputAddressId),
+                    AdoNetLayer.CreateInputParameter("currentTransactionOutputId", SqlDbType.BigInt, TransactionOutputCount));
+                SolveCount++;
+                if (SolveCount % 100000 == 0)
+                    Console.Write("\r已经处理了{0}个地址", SolveCount);
+                OutputAddressId++;
+            }
+        }
+
+        public SqlDataReader GetTransactionGraphEdgeReader(long bottomBound, long topBound)
+        {
+            string sqlGetEdge = @"
+                SELECT Edge.SourceId, Edge.TargetId, Edge.Value
+                FROM Edge
+                WHERE EdgeId >= @BottomBound AND EdgeId < @TopBound";
+
+            return this.adoNetLayer.ExecuteStatementReader(sqlGetEdge,
+                AdoNetLayer.CreateInputParameter("@BottomBound", SqlDbType.BigInt, bottomBound),
+                AdoNetLayer.CreateInputParameter("@TopBound", SqlDbType.BigInt, topBound));
+        }
     }
 }
--- Source/BitcoinTransactionGraphGenerator/BitcoinTransactionGraphGenerator/Program.cs
+++ Source/BitcoinTransactionGraphGenerator/BitcoinTransactionGraphGenerator/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BitcoinDataLayerAdoNet;
+using System.Data.SqlClient;
+using System.IO;
 
 namespace BitcoinTransactionGraphGenerator
 {
@@ -11,9 +13,20 @@ namespace BitcoinTransactionGraphGenerator
     {
         static void Main(string[] args)
         {
+            string FilePath = @"F:\Edge.csv";
             DatabaseConnection bc = DatabaseConnection.CreateSqlServerConnection(@"localhost\sq2", "BITCOIN");
             using (BitcoinAdoNetLayer bitcoinAdoNetLay = new BitcoinAdoNetLayer(bc.ConnectionString)) {
-                bitcoinAdoNetLay.GenerateEdgeByBatch();
+                SqlDataReader sr = bitcoinAdoNetLay.GetTransactionGraphEdgeReader(764746401, 766746401);
+                using(StreamWriter sw = File.CreateText(FilePath))
+                {
+                    sw.WriteLine("Source, Target, Value");
+                    while (sr.Read())
+                    {
+                        sw.WriteLine("{0}, {1}, {2}", sr["SourceId"], sr["TargetId"], sr["Value"]);
+                    }
+                    sw.Close();
+                }
+                sr.Close();
             }
         }
     }
